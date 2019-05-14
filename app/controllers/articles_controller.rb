2ad1823283271b5b27cf1dcca75780eacdd5f562
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-15 00:41:56 +0200

    delete acction, refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was successfully destroyed!'
+    redirect_to articles_path
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
