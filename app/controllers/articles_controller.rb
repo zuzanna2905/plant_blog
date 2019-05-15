a67bf47307e587fbe713235dfc06dfe1f2ba14ed
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-15 13:50:41 +0200

    style errors and messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,14 +14,14 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = 'Article was successfully destroyed!'
+    flash[:danger] = 'Article was successfully destroyed!'
     redirect_to articles_path
   end
 
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article was successfully created!'
+      flash[:success] = 'Article was successfully created!'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = 'Article was successfully updated!'
+      flash[:success] = 'Article was successfully updated!'
       redirect_to article_path(@article)
     else
       render 'edit'
