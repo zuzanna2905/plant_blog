b9fcb462b8b2f37f71af0760f94ef91db0d4c691
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-16 19:07:38 +0200

    show, listing, update, edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,38 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.all
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the plant blog #{@user}"
+      flash[:success] = "Welcome to the plant blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Your account was updated successfully'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
   private
 
   def user_params
