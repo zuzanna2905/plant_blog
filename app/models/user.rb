b40571bcd3507e8d30c6a02397752512fc84b03e
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-16 22:07:27 +0200

    admin functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # User model
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: {case_sensitive: false},
