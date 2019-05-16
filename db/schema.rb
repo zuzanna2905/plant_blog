6b455f4fc5608cb97c9ad3a5ee1e19efb3e63c5a
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-16 15:14:39 +0200

    sign up users from UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_120605) do
+ActiveRecord::Schema.define(version: 2019_05_16_123934) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_16_120605) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
