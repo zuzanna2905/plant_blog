18de869ed75f4bb1de0563ea74a6f4fdb5a07f77
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-16 14:22:29 +0200

    add association user - articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # User model
 class User < ApplicationRecord
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: {case_sensitive: false},
             length: { minimum: 3, maximum: 25}
